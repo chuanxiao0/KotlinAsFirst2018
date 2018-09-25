1db8c8950be5fea4d606b938d3714be19fb04a59
张传孝 <1071202612@qq.com>
2018-09-25 15:40:34 +0300

    first step 3

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -92,7 +92,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(s
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = {
+fun thirdDigit(number: Int): Int {
     val x = number % 1000
     return x/100
 }
@@ -105,7 +105,7 @@ fun thirdDigit(number: Int): Int = {
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = {
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int {
     val x = (hoursArrive * 60 - minutesArrive)
     val y = (hoursDepart * 60 - minutesArrive)
     return x - y
