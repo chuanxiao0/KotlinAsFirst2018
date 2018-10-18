90ab81842368f454781a2243f59dbd75fce08d92
chuanxiao0 <281358412@qq.com>
2018-10-18 23:22:56 +0300

    first step 3

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -102,7 +102,14 @@ fun fib(n: Int): Int {
  * Для заданных чисел m и n найти наименьшее общее кратное, то есть,
  * минимальное число k, которое делится и на m и на n без остатка
  */
-fun lcm(m: Int, n: Int): Int = TODO()
+fun lcm(m: Int, n: Int): Int {
+    var x = Math.max(n, m)
+    while (x in 1..n * m) {
+        if (x % n != 0 || x % m != 0) x += 1
+        else return x
+    }
+    return x
+}
 
 /**
  * Простая
@@ -125,7 +132,14 @@ fun maxDivisor(n: Int): Int = TODO()
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
+fun isCoPrime(m: Int, n: Int): Boolean {
+    var x = 1
+    for (i in 2..m) {
+        if (m % i ==0 && n % i == 0)
+            x = 0
+    }
+    return x != 0
+}
 
 /**
  * Простая
